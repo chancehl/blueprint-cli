134fef2a128dbd22c539fe709f47bd9ddb9691ca
Chance Linz <clinz.dev@gmail.com>
2023-03-30 20:07:11 -0700

    🚀 feat(Cargo.toml): add serde and serde_json dependencies 🚧 chore(create.rs): refactor handler function to use Blueprint struct 🆕 feat(react-component.json): add example blueprint for a React component
    🔍 chore(blueprint.rs): refactor Blueprint struct to include name and template fields
    ✨ feat(blueprint.rs): add seek method to search for blueprints by name in $HOME/.blueprint directory
    ✨ feat(blueprint.rs): add execute method to generate templates and write to disk or print to console

--- src/commands/create.rs
+++ src/commands/create.rs
@@ -1,16 +1,21 @@
 use clap::ArgMatches;
 use std::path::PathBuf;
 
+use crate::models::blueprint::Blueprint;
+
 pub fn handler(arg_matches: &ArgMatches) -> Result<(), &'static str> {
-    let blueprint = arg_matches
+    let blueprint_name = arg_matches
         .get_one::<String>("BLUEPRINT")
-        .expect("Blueprint required");
+        .expect("Blueprint name required");
 
     let destination = arg_matches.get_one::<PathBuf>("DESTINATION");
 
-    println!("blueprint={:?}, destination={:?}", blueprint, destination);
-
-    // validate blueprint file
+    match Blueprint::seek(blueprint_name.to_string()) {
+        Some(blueprint) => blueprint
+            .execute(destination)
+            .expect("Could not execute blueprint"),
+        None => todo!(),
+    };
 
     Ok(())
 }
--- src/models/blueprint.rs
+++ src/models/blueprint.rs
@@ -1,7 +1,16 @@
-use std::{fs, path::PathBuf};
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Blueprint {
-    contents: String,
+    template: String,
+    name: String,
     tokens: Vec<String>,
 }
 
@@ -12,5 +21,64 @@ impl From<&PathBuf> for Blueprint {
         println!("contents={:?}", contents);
 
         todo!()
+    }
+}
+
+impl Blueprint {
+    /// Searches for a blueprint with a given name in the $HOME/.blueprint directory
+    pub fn seek(name: String) -> Option<Blueprint> {
+        let blueprint_directory = PathBuf::from(format!(
+            "{}/.blueprint",
+            env::var("HOME").expect("Could not locate $HOME value")
+        ));
+
+        let local_templates =
+            fs::read_dir(blueprint_directory).expect("Could not read $HOME/.blueprint dir");
+
+        for template in local_templates {
+            let contents = &fs::read_to_string(template.unwrap().path())
+                .expect("Could not read template file");
+
+            let blueprint_json = serde_json::from_str::<Blueprint>(&contents);
+
+            if let Ok(blueprint) = blueprint_json {
+                if blueprint.name == name {
+                    return Some(blueprint);
+                }
+            };
+        }
+
+        None
+    }
+
+    pub fn execute(mut self, loc: Option<&PathBuf>) -> Result<()> {
+        // generate template
+        for token in &self.tokens {
+            let mut line = String::new();
+
+            println!("Enter value for {}:", token);
+
+            std::io::stdin().read_line(&mut line).unwrap();
+
+            let value = line
+                .strip_suffix("\n")
+                .unwrap_or(&line.to_string())
+                .to_owned();
+
+            self.template = self.template.replace(token, &value);
+        }
+
+        // write file to disk
+        if let Some(path) = loc {
+            let mut output =
+                File::create(path).expect(&format!("Could not create output file at {:?}", path));
+
+            write!(output, "{}", self.template)
+                .expect(&format!("Could not write to output file at {:?}", path));
+        } else {
+            println!("{:?}", self.template);
+        };
+
+        Ok(())
     }
 }
