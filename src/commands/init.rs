22a0e4def66c325d2909d6130350a40887f429e8
Chance Linz <clinz.dev@gmail.com>
2023-04-02 14:49:58 -0700

    🐛 fix(init.rs): simplify match statement to if statement

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -4,22 +4,19 @@ pub fn handler() -> Result<(), &'static str> {
     let home = env::var("HOME").expect("Could not locate $HOME value");
     let loc = format!("{}/.blueprint", home);
 
-    match PathBuf::from(&loc).exists() {
-        true => {
-            eprintln!(
-                ".blueprint directory already exists at {}, skipping initialization steps. If you want to force a fresh install, pass the `--force` flag to this command.",
-                loc
-            )
-        }
-        false => {
-            fs::create_dir(&loc).expect("Could not create .blueprint directory");
+    if PathBuf::from(&loc).exists() {
+        eprintln!(
+            ".blueprint directory already exists at {}, skipping initialization steps. If you want to force a fresh install, pass the `--force` flag to this command.",
+            loc
+        )
+    } else {
+        fs::create_dir(&loc).expect("Could not create .blueprint directory");
 
-            println!(
-                "Successfully initialized CLI. The following were created:\n* {:?}",
-                loc.to_string()
-            )
-        }
-    };
+        println!(
+            "Successfully initialized CLI. The following were created:\n* {:?}",
+            loc.to_string()
+        )
+    }
 
     Ok(())
 }
