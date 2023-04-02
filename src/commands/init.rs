bd266d13ae26d74a85743df045dd0fd620280661
Chance Linz <clinz.dev@gmail.com>
2023-04-02 12:47:19 -0700

    🐛 fix(init.rs): improve error message when .blueprint directory already exists ✨ feat(init.rs): add success message when initializing CLI and creating .blueprint directory

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -5,8 +5,20 @@ pub fn handler() -> Result<(), &'static str> {
     let loc = format!("{}/.blueprint", home);
 
     match PathBuf::from(&loc).exists() {
-        true => eprintln!(".blueprint directory already exists at {}", loc),
-        false => fs::create_dir(&loc).expect("Could not create .blueprint directory"),
+        true => {
+            eprintln!(
+                ".blueprint directory already exists at {}, skipping initialization steps. If you want to force a fresh install, pass the `--force` flag to this command.",
+                loc
+            )
+        }
+        false => {
+            fs::create_dir(&loc).expect("Could not create .blueprint directory");
+
+            println!(
+                "Successfully initialized CLI. The following were created:\n* {:?}",
+                loc.to_string()
+            )
+        }
     };
 
     Ok(())
